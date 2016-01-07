fe45f957a24f52d61d36a86336aafa19a4f00f76
Balancer <balancer@balancer.ru>
2016-01-07 15:03:41 +0300

    + ‘webroot/programming/php/cli.php’

---
+++ webroot/programming/php/cli.php
@@ -0,0 +1,12 @@
+# CLI
+
+## Обзоры и сравнения
+
+### Console Wars – PHP CLI Libraries
+
+Сравнение CLI-библиотек для PHP — Рассмотрены
+[symfony/Console](https://github.com/symfony/Console),
+[hoa/console](https://github.com/hoaproject/Console),
+[webmozart/console](https://github.com/webmozart/console).
+
+http://www.sitepoint.com/console-wars-php-cli-libraries/
